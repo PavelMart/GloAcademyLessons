c6c51e453d2c9be5ec8e325dd1bd3cacbb0a0297
Pavel Martakov <martackovp@yandex.ru>
2021-04-01 21:04:25 +0300

    Lesson 01 Task is solved

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert('hello');
+let money = 10000;
+let income = 1000;
+let addExpenses;
+let deposit = 5000;
+let mission = 'стать лучше';
+let period = 2;
+
+alert('Сообщение из первого урока!');
+
+console.log('А это вывод в консоль');
