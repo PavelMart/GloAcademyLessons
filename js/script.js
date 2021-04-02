9dd5a9a308d6a4d64fd35e9a5cff65270179b9db
Pavel Martakov <martackovp@yandex.ru>
2021-04-02 07:50:51 +0300

    Lesson02 Task is solved

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-let money = 10000;
-let income = 1000;
-let addExpenses;
-let deposit = 5000;
-let mission = 'стать лучше';
-let period = 2;
+let money = 30000;
+let income = 'Investments';
+let addExpenses = 'Food, Utility bills, Internet, Shopping, Gym';
+let deposit = true;
+let mission = 10000000;
+let period = 12;
+let budgetDay = money / 30;
 
-alert('Сообщение из первого урока!');
-
-console.log('А это вывод в консоль');
+console.log( typeof(money) );
+console.log( typeof(income) );
+console.log( typeof(deposit) );
+console.log(addExpenses.length);
+console.log( 'Период равен: ' + period + ' месяцев', 'Цель: заработать ' + mission + ' рублей');
+console.log( addExpenses.toLowerCase().split(', ') );
+console.log( 'Дневной бюджет: ' + budgetDay + ' рублей' );
